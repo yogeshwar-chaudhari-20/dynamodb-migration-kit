cd950c2f7f75aba8e24e1e6d9429182a4e16b1a7
yogeshwar-chaudhari-20 <chaudhari.yogesh20@gmail.com>
2024-09-08 05:30:45 +0930

    added example typescript project for easier setup process.

---
+++ example-typescript-project/src/migrations/20240901120131-first-migration.ts
@@ -0,0 +1,14 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+const TABLE_NAME = "YOUR_TABLE";
+const client = new DynamoDBClient({});
+
+export async function up() {
+  // TODO write your migration here.
+  console.log("first-migration: up successful");
+}
+
+export async function down() {
+  // TODO write the statements to rollback your migration
+  console.log("first-migration: down successful");
+}
